df6faa29fe23af18a7ac8cdb6b8449d4255b0699
mariana-pessoal <mariana.vns@gmail.com>
2023-02-04 06:52:21 -0300

    [feat] adds BikeClass

--- criacao-de-objetos/construtorParaCriarObjeto.js
+++ criacao-de-objetos/construtorParaCriarObjeto.js
@@ -9,7 +9,6 @@ const bicicleta = {
     ano: 2017,
     marca : "caloi"
 }
-
 console.log("A marca da bicicleta é " + bicicleta.marca)
 
 
@@ -22,7 +21,6 @@ function Bicicleta (cor, marchas, ano, marca){
     this.ano = ano;
     this.marca = marca
 }
-
 const bicicletaCriada = Bicicleta("azul", 10, 2015, "Caloi")
 console.log(bicicletaCriada)
 
@@ -36,3 +34,26 @@ console.log(bicicletaCriada)
             casa1 = new Casa("branca", 2);
             console.log(casa1)
             // Não funciona, pois Arrow Functions não podem ser construtores
+
+
+
+/* ### CRIANDO UM OBJETO PELA CLASSE ### */
+
+class BikeClass {
+    constructor(cor, marchas, marchaAtual) {
+        this.cor = cor;
+        this.marchas = marchas;
+        this.marchaAtual = marchaAtual
+    }
+    mudarMarcha(direcao){
+        if (direcao === "paraCima" && marchaAtual < marchas) {
+            marchaAtual =+ 1
+        }      
+        if (direcao === "paraBaixo" && marchaAtual > 1){
+            marchaAtual =- 1
+        }
+        console.log("Marcha atual: " + marchaAtual)
+    }
+}
+const bicicletaDaClasse = new Bike ("branca", 10, 2);
+bicicletaDaClasse.mudarMarcha("paraCima")
